c8e43b1f402bbe9172962e6121f6860ecaca3c1a
Subin K Kuriakose <subinkk08@gmail.com>
2023-02-09 21:40:03 -0400

    Created Ride Project

---
+++ Rides/Rides/Rides/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rides
+//
+//  Created by Subin Kuriakose on 09/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
